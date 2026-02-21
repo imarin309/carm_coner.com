010c7bb4c03e3905fa6adea9ef8dc7929341484c
imarin309 <16imarin309@gmail.com>
2026-02-21 22:07:08 +0900

    feat: json-ldを追加

--- src/constants/meta.ts
+++ src/constants/meta.ts
@@ -3,3 +3,4 @@ export const siteCatchCopy = "模型筆塗りの楽しさを広めたい";
 export const siteDescription =
   "ガンプラや美プラを筆塗りで塗装するホビーブログです。制作過程や塗装テクニックを写真付きで紹介します。";
 export const siteUrl = "https://calm-corner.com";
+export const siteAuthor = "imarin";
